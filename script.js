3171a7971e793ba3e9499003e0cb8da5a285bdc2
Ann M Hagan <39930638+anniepuy@users.noreply.github.com>
2023-09-01 16:12:50 -0500

    adding email validation

--- script.js
+++ script.js
@@ -12,6 +12,17 @@ function showError(input, message){
     small.innerText = message;
 }
 
+
+function isValidEmail (email) {
+    const validateEmail = (email) => {
+    return String(email)
+        .toLowerCase()
+          .match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+          );
+      };
+}
+
 // Show success outline
 function showSuccess(input) {
     const formControl = input.parentElement;
@@ -26,6 +37,26 @@ form.addEventListener('submit', function(e) {
         showError(username, 'Username is required')
     } else {
         showSuccess(username);
+    }
+
+    if (email.value === '') {
+        showError(email, 'Email is required');
+    } else if (!isValidEmail(email.value)) {
+        showError(email, 'Email is not valid')   
+    } else {
+        showSuccess(email);
+    }
+
+    if(password.value === '') {
+        showError(password, 'Password is required')
+    } else {
+        showSuccess(password);
+    }
+
+    if(password2.value === '') {
+        showError(password2, 'Password is required')
+    } else {
+        showSuccess(password2);
     }
 })
 
