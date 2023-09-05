ad19d9bfeb68ff89cad34593bac2ad76c6272858
Ann M Hagan <39930638+anniepuy@users.noreply.github.com>
2023-09-05 03:55:13 -0500

    fixed CSS

--- script.js
+++ script.js
@@ -5,58 +5,60 @@ const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
 // Functions
-function showError(input, message){
+function showError(input, message) {
     const formControl = input.parentElement;
     formControl.className = 'form-control error';
     const small = formControl.querySelector('small');
     small.innerText = message;
 }
 
-
-function isValidEmail (email) {
-    const validateEmail = (email) => {
-    return String(email)
-        .toLowerCase()
-          .match(
-            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-          );
-      };
+function showSuccess(input, message) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
 }
 
-// Show success outline
-function showSuccess(input) {
-    const formControl = input.parentElement;
-    formControl.className = 'form-control success'
+function checkEmail (input) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(re.test(input.value.trim)) {
+        showSuccess(input)
+    } else {
+        showError(input, 'Email is not valid.');
+    }
+}
+
+// Validate required fields
+function checkRequired(inputArr) {
+    inputArr.forEach(function(input) {
+        if(input.value.trim() === '') {
+            showError(input, `${getFieldName(input)} is required.`);
+        } else {
+            showSuccess(input);
+        }
+    });
+}
+
+//Check length of inputs
+function checkLength(input, min, max) {
+    if(input.value.length < min) {
+        showError(input, `${getFieldName(input)} must be at least ${min} and max of ${max} characters.`);
+    } else if (input.value.length > max) {
+            showError(input, `${getFieldName(input)} must be at no more than ${max} characters.`);
+    }else {
+        showSuccess(input);
+    }
+}
+
+// Get fieldname function
+function getFieldName(input) {
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
 
 //Event listeners
 form.addEventListener('submit', function(e) {
     e.preventDefault();
-
-    if(username.value === '') {
-        showError(username, 'Username is required')
-    } else {
-        showSuccess(username);
-    }
-
-    if (email.value === '') {
-        showError(email, 'Email is required');
-    } else if (!isValidEmail(email.value)) {
-        showError(email, 'Email is not valid')   
-    } else {
-        showSuccess(email);
-    }
-
-    if(password.value === '') {
-        showError(password, 'Password is required')
-    } else {
-        showSuccess(password);
-    }
-
-    if(password2.value === '') {
-        showError(password2, 'Password is required')
-    } else {
-        showSuccess(password2);
-    }
+    checkRequired([username, email, password, password2]);
+    checkLength(username, 3, 18);
+    checkLength(password, 12, 24);
+    checkEmail(email);
 })
 
